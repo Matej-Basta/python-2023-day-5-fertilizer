653044bec07e639d4383fa6c05bb8f301793c631
matej <matej.basta@email.cz>
2024-07-19 14:20:34 +0200

    helper function

--- firstchallenge.py
+++ firstchallenge.py
@@ -7,6 +7,12 @@ def main():
 
     try:
         with open(args.input_file, "r") as input_file:
+            almanac = extract_numbers(input_file)
+            
+            print(almanac)
+            print("--------------")
+
+
             soil = [[50, 98, 2], [52, 50, 48]]
             seed_soil_map = {}
             for list in soil:
@@ -18,6 +24,55 @@ def main():
         raise Exception("The file does not exist.")
     except:
         raise Exception("Something went wrong.")
+
+def extract_numbers(input_file):
+    almanac = {}
+    current = None
+    for line in input_file:
+        if line == "\n":
+            continue
+        elif "seeds" in line:
+            almanac["seeds"] = line.split(":")[-1].split()
+        elif "seed-to-soil" in line:
+            current = "soil"
+            continue
+        elif "soil-to-fertilizer" in line:
+            current = "fertilizer"
+            continue
+        elif "fertilizer-to-water" in line:
+            current = "water"
+            continue
+        elif "water-to-light" in line:
+            current = "light"
+            continue
+        elif "light-to-temperature" in line:
+            current = "temperature"
+            continue
+        elif "temperature-to-humidity" in line:
+            current = "humidity"
+            continue
+        elif "humidity-to-location" in line:
+            current = "location"
+            continue
+        else:
+            match current:
+                case "soil":
+                    almanac["seed_to_soil"] = line.split()
+                case "fertilizer":
+                    almanac["soil_to_fertilizer"] = line.split()
+                case "water":
+                    almanac["fertilizer_to_water"] = line.split()
+                case "light":
+                    almanac["water_to_light"] = line.split()
+                case "temperature":
+                    almanac["light_to_temperature"] = line.split()
+                case "humidity":
+                    almanac["temperature_to_humidity"] = line.split()
+                case "location":
+                    almanac["humidity_to_location"] = line.split()
+
+    return almanac
+
 
 def get_soil(seed, seed_soil_map):
     if soil := seed_soil_map.get(seed):
