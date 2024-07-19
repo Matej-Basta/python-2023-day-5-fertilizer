013306b16a5f565bb26b61ee32a61197840d9ccb
matej <matej.basta@email.cz>
2024-07-19 12:44:06 +0200

    arguments and reading from file prepared

--- firstchallenge.py
+++ firstchallenge.py
@@ -1,12 +1,23 @@
+import argparse
+
 def main():
-    soil = [[50, 98, 2], [52, 50, 48]]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    args = parser.parse_args()
 
-    seed_soil_map = {}
-    for list in soil:
-        for i in range(list[-1]):
-            seed_soil_map[list[1] + i] = list[0] + i
+    try:
+        with open(args.input_file, "r") as input_file:
+            soil = [[50, 98, 2], [52, 50, 48]]
+            seed_soil_map = {}
+            for list in soil:
+                for i in range(list[-1]):
+                    seed_soil_map[list[1] + i] = list[0] + i
 
-    print(get_soil(98, seed_soil_map))
+            print(get_soil(98, seed_soil_map))
+    except FileNotFoundError:
+        raise Exception("The file does not exist.")
+    except:
+        raise Exception("Something went wrong.")
 
 def get_soil(seed, seed_soil_map):
     if soil := seed_soil_map.get(seed):
