520d89fd5436113bdb98428b966e08d29cb42127
matej <matej.basta@email.cz>
2024-07-22 17:01:07 +0200

    first challenge but very slow

--- firstchallenge.py
+++ firstchallenge.py
@@ -9,17 +9,14 @@ def main():
         with open(args.input_file, "r") as input_file:
             almanac = extract_numbers(input_file)
             
-            print(almanac)
-            print("--------------")
+            smallest_location = float("inf")
+            for seed in almanac["seeds"]:
+                location = get_location(seed, almanac)
+                smallest_location = location if location < smallest_location else smallest_location
+            
+            print(smallest_location)
 
 
-            soil = [[50, 98, 2], [52, 50, 48]]
-            seed_soil_map = {}
-            for list in soil:
-                for i in range(list[-1]):
-                    seed_soil_map[list[1] + i] = list[0] + i
-
-            print(get_soil(98, seed_soil_map))
     except FileNotFoundError:
         raise Exception("The file does not exist.")
     except:
@@ -29,48 +26,63 @@ def extract_numbers(input_file):
     almanac = {}
     current = None
     for line in input_file:
+        print("here")
         if line == "\n":
             continue
         elif "seeds" in line:
-            almanac["seeds"] = line.split(":")[-1].split()
+            almanac["seeds"] = [int(x) for x in line.split(":")[-1].split()]
         elif "seed-to-soil" in line:
+            almanac["seed_to_soil"] = {}
             current = "soil"
             continue
         elif "soil-to-fertilizer" in line:
+            almanac["soil_to_fertilizer"] = {}
             current = "fertilizer"
             continue
         elif "fertilizer-to-water" in line:
+            almanac["fertilizer_to_water"] = {}
             current = "water"
             continue
         elif "water-to-light" in line:
+            almanac["water_to_light"] = {}
             current = "light"
             continue
         elif "light-to-temperature" in line:
+            almanac["light_to_temperature"] = {}
             current = "temperature"
             continue
         elif "temperature-to-humidity" in line:
+            almanac["temperature_to_humidity"] = {}
             current = "humidity"
             continue
         elif "humidity-to-location" in line:
+            almanac["humidity_to_location"] = {}
             current = "location"
             continue
         else:
+            data = [int(x) for x in line.split()]
             match current:
                 case "soil":
-                    almanac["seed_to_soil"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["seed_to_soil"][data[1] + i] = data[0] + i
                 case "fertilizer":
-                    almanac["soil_to_fertilizer"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["soil_to_fertilizer"][data[1] + i] = data[0] + i
                 case "water":
-                    almanac["fertilizer_to_water"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["fertilizer_to_water"][data[1] + i] = data[0] + i
                 case "light":
-                    almanac["water_to_light"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["water_to_light"][data[1] + i] = data[0] + i
                 case "temperature":
-                    almanac["light_to_temperature"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["light_to_temperature"][data[1] + i] = data[0] + i
                 case "humidity":
-                    almanac["temperature_to_humidity"] = line.split()
+                    for i in range(data[-1]):
+                        almanac["temperature_to_humidity"][data[1] + i] = data[0] + i
                 case "location":
-                    almanac["humidity_to_location"] = line.split()
-
+                    for i in range(data[-1]):
+                        almanac["humidity_to_location"][data[1] + i] = data[0] + i
     return almanac
 
 
@@ -79,6 +91,48 @@ def get_soil(seed, seed_soil_map):
         return soil
     else:
         return seed
+
+def get_fertilizer(soil, soil_fertilizer_map):
+    if fertilizer := soil_fertilizer_map.get(soil):
+        return fertilizer
+    else:
+        return soil
+
+def get_water(fertilizer, fertilizer_water_map):
+    if water := fertilizer_water_map.get(fertilizer):
+        return water
+    else:
+        return fertilizer
+
+def get_light(water, water_light_map):
+    if light := water_light_map.get(water):
+        return light
+    else:
+        return water
+
+def get_temperature(light, light_temperature_map):
+    if temperature := light_temperature_map.get(light):
+        return temperature
+    else:
+        return light
+
+def get_humidity(temperature, temperature_humidity_map):
+    if humidity := temperature_humidity_map.get(temperature):
+        return humidity
+    else:
+        return temperature
+
+def get_location(seed, almanac):
+    soil = get_soil(seed, almanac["seed_to_soil"])
+    fertilizer = get_fertilizer(soil, almanac["soil_to_fertilizer"])
+    water = get_water(fertilizer, almanac["fertilizer_to_water"])
+    light = get_light(water, almanac["water_to_light"])
+    temperature = get_temperature(light, almanac["light_to_temperature"])
+    humidity = get_humidity(temperature, almanac["temperature_to_humidity"])
+    if location := almanac["humidity_to_location"].get(humidity):
+        return location
+    else:
+        return humidity
 
 if __name__ == "__main__":
     main()
